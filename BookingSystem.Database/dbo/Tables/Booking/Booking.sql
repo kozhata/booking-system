8d9839a2d1541423672f1ab7ba9e699e9aceedba
Aleksandar Kozhinkov <ako@mca.dev>
2022-12-06 16:34:56 +0100

    Added database project and table

---
+++ BookingSystem.Database/dbo/Tables/Booking/Booking.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Booking] (
+    [Id]                INT             IDENTITY (1, 1) NOT NULL,
+    [CreatedOn]         SMALLDATETIME   NOT NULL,
+    [Name]              NVARCHAR(50)    NOT NULL,
+    [Flexibility]       NVARCHAR(10)    NOT NULL,
+    [VehicleSize]       NVARCHAR(10)    NOT NULL,
+    [ContactNumber]     NVARCHAR(20)    NOT NULL,
+    [Email]             NVARCHAR(50)    NOT NULL,
+    CONSTRAINT [PK_Booking] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
